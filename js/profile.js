91b72698f8882c27f40a403e61f821ae54c843ed
Naveen Kumar <naveenpulikondaaiee@gmail.com>
2019-12-18 18:47:56 +0530

    'test'

--- js/profile.js
+++ js/profile.js
@@ -39,15 +39,25 @@ function userDetails(user) {
 
     });
     // //--------------user pack details-------------------------------------------------------//
-    // db.collection('userDetails').doc('gp2HvuSGZQOt7PiDrJAmBpjye0k1').collection('selectedChannels').get().then(doc => {
-    //         doc.forEach(e => {
-    //             $('#pack-name').text(e.data().PackName)
-    //             $('#pack-price').text(e.data().Price)
-    //             $('#pack-date').text(e.data().Date)
-    //             $('#pack-id').text(e.data().PaymentId)
-    //         })
-    //     })
-    //-------------------------user information update modal--------------------------------//
+    db.collection('userDetails').doc(user.uid).collection('selectedChannels').get().then(doc => {
+            doc.forEach(e => {
+
+                // $('#pack-name').text(e.data().PackName)
+                // $('#pack-price').text(e.data().Price)
+                // $('#pack-date').text(e.data().Date)
+                // $('#pack-id').text(e.data().PaymentId)
+                let packHtml = ` <div class="card bg-c-blue order-card">
+                <div class="card-block">
+                    <h6 class="m-b-20" id="pack-date">${e.data().Date}</h6>
+                    <h2 class="text-left" id="pack-name">${e.data().PackName}<i class="fa fa-credit-card text-right"></i> </h2>
+                    <p class="m-b-0 text-white" id="pack-date">${e.data().PaymentId}<span class="f-right text-white" id="pack-price">${e.data().Price}/month</span></p>
+                </div>
+            </div>`;
+                $('.pack-info-append-container').append(packHtml);
+            })
+
+        })
+        //-------------------------user information update modal--------------------------------//
     $('#profile-update').on('click', function(e) {
         e.preventDefault();
 
@@ -55,7 +65,7 @@ function userDetails(user) {
         var address = $('#u-address').val();
         var email = $('#u-email').val();
         var contact = $('#u-contact').val();
-        db.collection('userDetails').doc('gp2HvuSGZQOt7PiDrJAmBpjye0k1').set({
+        db.collection('userDetails').doc(user.uid).set({
             name: name,
             address: address,
             email: email,
