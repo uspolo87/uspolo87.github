b1e716bb7a98a6867b4cddf208c32a5943788dc5
Naveen Kumar <naveenpulikondaaiee@gmail.com>
2019-12-21 18:17:21 +0530

    'friday-code'

--- js/profile.js
+++ js/profile.js
@@ -3,9 +3,14 @@ $(document).ready(function() {
     // $('.preloader').css('display', 'none');
 
     var currentUserId;
+    var finalImageUrl;
     var userProfileContainer = document.querySelector('.profile');
-    console.log(userProfileContainer);
     var logoutContainer = document.querySelector('.user-logout-container');
+    // //firebase storage ref
+    // var storage = firebase.storage().ref();
+    // var imagesRef = storage.child('userProfileImages');
+
+
     auth.onAuthStateChanged(user => {
         if (user) {
             userProfileContainer.style.display = "block";
@@ -40,27 +45,58 @@ function userDetails(user) {
     });
     // //--------------user pack details-------------------------------------------------------//
     db.collection('userDetails').doc(user.uid).collection('selectedChannels').get().then(doc => {
-            doc.forEach(e => {
+        doc.forEach(e => {
 
-                // $('#pack-name').text(e.data().PackName)
-                // $('#pack-price').text(e.data().Price)
-                // $('#pack-date').text(e.data().Date)
-                // $('#pack-id').text(e.data().PaymentId)
-                let packHtml = ` <div class="card bg-c-blue order-card">
+            // $('#pack-name').text(e.data().PackName)
+            // $('#pack-price').text(e.data().Price)
+            // $('#pack-date').text(e.data().Date)
+            // $('#pack-id').text(e.data().PaymentId)
+            let packHtml = ` <div class="card bg-c-blue order-card">
                 <div class="card-block">
                     <h6 class="m-b-20" id="pack-date">${e.data().Date}</h6>
                     <h2 class="text-left" id="pack-name">${e.data().PackName}<i class="fa fa-credit-card text-right"></i> </h2>
                     <p class="m-b-0 text-white" id="pack-date">${e.data().PaymentId}<span class="f-right text-white" id="pack-price">${e.data().Price}/month</span></p>
                 </div>
             </div>`;
-                $('.pack-info-append-container').append(packHtml);
-            })
-
+            $('.pack-info-append-container').append(packHtml);
         })
-        //-------------------------user information update modal--------------------------------//
+
+    })
+
+    //uploading user image to firebase storage and retrieving url;
+    var userUploadImg = document.querySelector('#u-img');
+    userUploadImg.addEventListener('change', (event) => {
+
+
+        let uploadImage = document.querySelector('input[type=file]').files[0];;
+        var reader = new FileReader();
+        reader.readAsDataURL(uploadImage);
+        let storageRef = firebase.storage().ref('/userProfileImages/' + uploadImage.name);
+        let task = storageRef.put(uploadImage);
+        task.on('state_changed', (snapshot) => {
+            let uploadPercentage = snapshot.bytesTransferred / snapshot.totalBytes * 100;
+            console.log('upload is' + uploadPercentage + "done");
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                    console.log('Upload is paused');
+                    break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                    console.log('Upload is running');
+                    break;
+            }
+        }, function(error) {
+            console.log('upload is unsuccessful')
+        }, function() {
+            task.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                finalImageUrl = downloadURL;
+            })
+        })
+    })
+
+    //-------------------------user information update modal--------------------------------//
     $('#profile-update').on('click', function(e) {
         e.preventDefault();
-
+        var userImgUrl = finalImageUrl;
         var name = $('#u-name').val();
         var address = $('#u-address').val();
         var email = $('#u-email').val();
@@ -69,13 +105,11 @@ function userDetails(user) {
             name: name,
             address: address,
             email: email,
-            contact: contact
+            contact: contact,
+            url: userImgUrl
         }).then(e => {
             console.log('successfully updated');
             window.location.reload();
         })
     })
 }
-
-
-console.log(Notification.permission);
